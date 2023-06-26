2db0d80721d33b784755288baaf5950aa8ead6fd
Shubham <malhotrashubham726@gmail.com>
2023-06-26 16:51:34 +0530

    adding title and reorganizing code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 export default function Navbar(props) {
+
+    let location = useLocation();
+
+    useEffect(() => {
+        console.log(location.pathname);
+    },[location]) 
+
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
     <div className="container-fluid">
@@ -14,16 +21,12 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/textwork">Home</Link>
+            <Link className={`nav-link ${location.pathname === "/textwork" ? "active" : " "}`} aria-current="page" to="/textwork">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+            <Link className={`nav-link ${location.pathname === "/about" ? "active" : " "}`} to="/about">{props.aboutText}</Link>
           </li>
         </ul>
-        {/*<form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-primary" type="submit">Search</button>
-        </form>*/}
         <div className={`form-check form-switch text-${(props.mode)==="light"? "dark" : "light"}`}>
           <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault"/>
           <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {(props.mode)==="light"? "dark" : "light"}mode</label>
